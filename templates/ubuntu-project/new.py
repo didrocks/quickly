b4ea7bba4043344df38d2c35696afd5cd28426d2
Didier Roche <didrocks@ubuntu.com>
2009-05-27 00:23:52 +0200

    add bzr initial repository setup

--- templates/ubuntu-project/new.py
+++ templates/ubuntu-project/new.py
@@ -4,31 +4,32 @@ import shutil
 import subprocess
 
 def file_from_template(template_dir, template_file, target_dir, project_name, rename = False):
- if rename:
-  frags = template_file.split(".")
-  target_file = project_name
-  if len(frags) > 1:
-   target_file += "." + frags[1]
- else:
-  target_file = template_file
+  if rename:
+   frags = template_file.split(".")
+   target_file = project_name
+   if len(frags) > 1:
+     target_file += "." + frags[1]
+  else:
+    target_file = template_file
 
- print "Creating %s" % (target_dir + "/" + target_file,)
- fin = open(template_dir + template_file,'r')
- file_contents1 = fin.read().replace("project_name",project_name)
- fout = open(target_dir + target_file, 'w')
- fout.write(file_contents1)
- fout.flush()
- fout.close()
- fout.close()
- print  "%s created\n" % (target_dir + "/" + target_file,)
+  print "Creating %s" % target_dir + "/" + target_file
+  fin = open(template_dir + template_file,'r')
+  file_contents1 = fin.read().replace("project_name",project_name)
+  fout = open(target_dir + target_file, 'w')
+  fout.write(file_contents1)
+  fout.flush()
+  fout.close()
+  fout.close()
+  print  "%s created\n" % (target_dir + "/" + target_file,)
  
+
+
 #get the name of the project
 if len(sys.argv)< 2:
- print ""
- print "Error, project name not defined"
- print "usage is xxxx"
- print "Aborting"
- sys.exit(0)
+  print ""
+  print "Error, project name not defined. Usage is project_name"
+  print "Aborting"
+  sys.exit(0)
 
 pathname = os.path.dirname(sys.argv[0])
 abs_path =  os.path.abspath(pathname)
@@ -69,13 +70,14 @@ shutil.copy2(template_media_dir + "background.png",target_media_dir)
 shutil.copy2(template_media_dir + "logo.png",target_media_dir)
 print "Media files copied to %s\n" % target_media_dir
 
-#set permissions
-
-#run the program
-
-#print next steps
+print "Creating bzr repository and commiting"
+subprocess.call(["bzr", "init"], cwd=project_name)
+subprocess.call(["bzr", "add"], cwd=project_name)
+subprocess.call(["bzr", "commit", "-m 'initial project creation'"], cwd=project_name)
 
 print "finishing"
+
+#run the program
 subprocess.call(["python",project_name + ".py"], cwd=project_name + "/python/")
 
 sys.exit(0)
