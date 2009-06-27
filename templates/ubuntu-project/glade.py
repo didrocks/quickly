1767658cb9377bc5e8e31cf6f3e720cab22e783f
Rick Spencer <rick.spencer@canonical.com>
2009-06-27 13:13:51 -0700

    created help.py

--- templates/ubuntu-project/glade.py
+++ templates/ubuntu-project/glade.py
@@ -1,10 +1,6 @@
 import os
 import subprocess
 
-# workaround as this doesn't work:
-#os.environ['GLADE_CATALOG_PATH'] = './ui'
-#subprocess.Popen(["glade-3", "ui/*.ui"], shell=True)
-
 if os.getenv('QUICKLY') is not None and "verbose" in os.getenv('QUICKLY').lower():
     subprocess.Popen("GLADE_CATALOG_PATH=./ui glade-3 ui/*.ui", shell=True)
 else:
---
+++ templates/ubuntu-project/help.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open("help/index.html")
