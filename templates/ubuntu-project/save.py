777f2024c5e626501857580abb759cdc077c5d50
Rick Spencer <rick.spencer@canonical.com>
2009-06-27 14:18:57 -0700

    made quickly save work better, and added it to the tutorial.

--- templates/ubuntu-project/save.py
+++ templates/ubuntu-project/save.py
@@ -1,7 +1,9 @@
 import sys
 import subprocess
 
-comment = sys.argv[1]
+comment = "quickly saved"
+if sys.argv[1] != "":
+    comment = sys.argv[1]
 
 subprocess.call(["bzr", "add"])
 subprocess.call(["bzr", "commit", "-m",   comment])
