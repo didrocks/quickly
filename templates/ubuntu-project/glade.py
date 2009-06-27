7ff02032fcb632d251195716309ee86804bd4690
Rick Spencer <rick.spencer@canonical.com>
2009-06-27 15:35:13 -0700

    change catalogue files to not put windows and dialogs in the palette

--- templates/ubuntu-project/glade.py
+++ templates/ubuntu-project/glade.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
 
+
+
+#run glade with env variables pointing to catalogue xml files
 if os.getenv('QUICKLY') is not None and "verbose" in os.getenv('QUICKLY').lower():
     subprocess.Popen("GLADE_CATALOG_PATH=./ui glade-3 ui/*.ui", shell=True)
 else:
