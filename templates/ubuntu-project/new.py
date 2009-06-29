45c9aa90ef7b4257a1af05504f5f68674452d1ad
Didier Roche <didrocks@ubuntu.com>
2009-06-29 23:07:19 +0200

    * Reshape project folder handling :   • cwd for each command corresponds to the project path (where .quickly file is)   • give it as a parameter for hooks (pre_foo, post-foo)   • better error handling   • reshape new.py in ubuntu-project to be compatible :) * Refactor and cleaning configuration handling   • remove ugly // in new.py and dialog.py * clean dialog.py absolute path name is not needed (yes, it's cwd!)

--- templates/ubuntu-project/new.py
+++ templates/ubuntu-project/new.py
@@ -26,12 +26,12 @@ abs_path = os.path.abspath(pathname)
 project_name = quicklyutils.quickly_name(sys.argv[1])
 
 # create additional directories
-ui_dir = project_name + "/ui"
+ui_dir = "ui"
 print _("Creating project directory %s") % ui_dir
 os.mkdir(ui_dir)
 print _("Directory %s created\n") % ui_dir
 
-python_dir = project_name + "/python"
+python_dir = "python"
 print _("Creating project directory %s") % python_dir
 os.mkdir(python_dir)
 print _("Directory %s created\n") % python_dir
@@ -41,7 +41,7 @@ sentence_name, camel_case_name = quicklyutils.conventional_names(project_name)
 
 #copy files
 template_ui_dir = abs_path + "/ui/"
-target_ui_dir = project_name + "/ui/"
+target_ui_dir = "ui"
 
 substitutions = (("project_name",project_name),
             ("camel_case_name",camel_case_name),
@@ -56,32 +56,32 @@ quicklyutils.file_from_template(template_ui_dir, "camel_case_namePreferencesDial
 quicklyutils.file_from_template(template_ui_dir, "project_name_preferences_dialog.xml", target_ui_dir, substitutions)
 
 template_python_dir = abs_path + "/python/"
-target_python_dir = project_name + "/python/"
+target_python_dir = "python"
 quicklyutils.file_from_template(template_python_dir, "camel_case_nameWindow.py", target_python_dir, substitutions)
 quicklyutils.file_from_template(template_python_dir, "Aboutcamel_case_nameDialog.py", target_python_dir, substitutions)
 quicklyutils.file_from_template(template_python_dir, "camel_case_namePreferencesDialog.py", target_python_dir, substitutions)
 
 template_media_dir = abs_path + "/media/"
-target_media_dir = project_name + "/media/"
+target_media_dir = "media"
 shutil.copytree(template_media_dir,target_media_dir)
 
-template_help_dir = abs_path + "/help"
-target_help_dir = project_name + "/help"
+template_help_dir = abs_path + "/help/"
+target_help_dir = "help"
 shutil.copytree(template_help_dir,target_help_dir)
 
 
 #(template_dir, template_file, target_dir, project_name, rename = False):
-quicklyutils.file_from_template(abs_path,"/internal/project_name",project_name, substitutions)
+quicklyutils.file_from_template(abs_path, "/internal/project_name", '.', substitutions)
 
 print _("Creating bzr repository and commiting")
-subprocess.call(["bzr", "init"], cwd=project_name)
-subprocess.call(["bzr", "add"], cwd=project_name)
-subprocess.call(["bzr", "commit", "-m",    "initial project creation"], cwd=project_name)
+subprocess.call(["bzr", "init"])
+subprocess.call(["bzr", "add"])
+subprocess.call(["bzr", "commit", "-m", "Initial project creation"])
 
 print _("Launching a first demo")
 
 #run the program
-subprocess.call(["python", camel_case_name + "Window.py"], cwd=project_name + "/python/")
+subprocess.call(["python", camel_case_name + "Window.py"], cwd="python/")
 
 print _("Congrats, your new project is setup! You can now cd %s and start hacking.") % project_name
 
