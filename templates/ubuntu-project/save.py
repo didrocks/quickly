97cd1ad378ffcf720eb66ec3f2f6a7d1c389bfc2
Rick Spencer <rick.spencer@canonical.com>
2009-06-29 15:41:31 -0700

    made save command more easy and fun. You can just type all kinds of stuff after 'save' and it will add it all as a comment

--- templates/ubuntu-project/save.py
+++ templates/ubuntu-project/save.py
@@ -11,17 +11,26 @@ if os.path.exists('/usr/share/quickly/builtins'):
 builtins_directory =  os.path.abspath(builtins_directory)
 sys.path.append(builtins_directory)
 
-
 import gettext
 from gettext import gettext as _
-
 gettext.textdomain('quickly')
 
-commit_msg = _('quickly saved')
-
+#set either a default message or the specified message
+commit_msg = ""
 if len(sys.argv) > 1:
- commit_msg = sys.argv[1]
+ i = 1
+ while i < len(sys.argv):
+  if i > 1:
+   commit_msg += " "
+  commit_msg += sys.argv[i]
+  i += 1
 
+if commit_msg == "":
+ commit_msg = _('quickly saved')
+
+print commit_msg
+
+#save away
 subprocess.call(["bzr", "add"])
 subprocess.call(["bzr", "commit", "-m" + commit_msg])
 
