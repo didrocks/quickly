54eaad5ee8dfc8f0112bb85c5ac9fa72d6c96277
Rick Spencer <rick.spencer@canonical.com>
2009-06-29 14:38:21 -0700

    removing save command, as didrocks already implimented it better

--- templates/ubuntu-project/save.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-
-comment = "quickly saved"
-if sys.argv[1] != "":
-    comment = sys.argv[1]
-
-subprocess.call(["bzr", "add"])
-subprocess.call(["bzr", "commit", "-m",   comment])
