2d6fe6405a9007b0cdff22fb3888cdf9433685b7
Didier Roche <didrocks@ubuntu.com>
2009-07-03 00:33:35 +0200

    * "quickly release" released!   - $ quickly release useis a default commit commentary "quickly released"       and doesn't tag the branch (it will after, retreiving it from setup.py       if it exists   - $ quickly release "release-name" tags the branch with release-name       and add a default commit message   - $ quickly release "release-name" wathever you want to write       tags the branch with release-name and use "wathever you want to write"       as the commit commentary   - it can uses staging and production launchpad, as usual   - switching from production to staging launchpad environment is supported   - opens a browser showing the branch (same, staging or prod supported)   - prevent to tag 2 releases with the same name * little typo fix in quickly new and return code checked in quickly save

--- templates/ubuntu-project/release.py
+++ templates/ubuntu-project/release.py
@@ -3,6 +3,8 @@
 import os
 import sys
 import subprocess
+import webbrowser
+
 # default to looking up lpi integration related to the current dir
 pathname = os.path.dirname(sys.argv[0])
 builtins_directory = pathname + '/../../builtins/'
@@ -10,47 +12,101 @@ if os.path.exists('/usr/share/quickly/builtins'):
     builtins_directory = '/usr/share/quickly/builtins'
 builtins_directory =  os.path.abspath(builtins_directory)
 sys.path.append(builtins_directory)
-
+import launchpadaccess
 
 import gettext
 from gettext import gettext as _
-
-from optparse import OptionParser
-import sys
-
-#set domain text
 gettext.textdomain('quickly')
 
-# parse entry parameter
-option_message = {'name': ('-m', '--message'), 'dest': 'commit_message',
-                  'help': _('Description of the new revision')}
 
-options = [option_message]
-parser = OptionParser()
-for option in options:
-    param = option['name']
-    del option['name']
-    parser.add_option(*param, **option)
-options, args = parser.parse_args()
+args = sys.argv
 
-if len(args) < 1:
-    print _("""
-ERROR: Release command must have a release name""")
-    sys.exit(1)
+if len(args) == 1:
+    # TODO: try to guess new release name in setup.py
+    release_name = ""
+    commit_msg = _('quickly released')
+elif len(args) == 2:
+    release_name = args[1]
+    commit_msg = _('quickly released: %s' % release_name)
+elif len(args) > 2:
+    release_name = args[1]
+    commit_msg = " ".join(args[2:])
+    
+launchpad = None
+project = None
 
 # connect to LP
-import launchpadaccess
 launchpad = launchpadaccess.initialize_lpi()
 
 # get the project
-launchpadaccess.get_project(launchpad)
+project = launchpadaccess.get_project(launchpad)
 
-# commit and push !
-additional_bzr_option = ['-m', _('Releasing %s') % " ".join(args)]
-if options.commit_message:
-    additional_bzr_option = ['-m'] + [options.commit_message]
-subprocess.call(["bzr", "commit"] + additional_bzr_option)
+    
+# check if already released with this name
+if release_name: # TODO: remove test when release detected
+    bzr_instance = subprocess.Popen(["bzr", "tags"], stdout=subprocess.PIPE)
+    bzr_tags = bzr_instance.stdout.read()
+    if release_name in bzr_tags:
+        print _("ERROR: quickly can't release: %s seems to be already released. Choose another name.") % release_name
+        sys.exit(1)
+        
+    
+# add files, commit and push !
+subprocess.call(["bzr", "add"])
+return_code = subprocess.call(["bzr", "commit", '-m', commit_msg])
+if return_code != 0:
+    print _("ERROR: quickly can't release as it can't commit. Are you sure you made any changes?")
+    sys.exit(1)
 
-subprocess.call(["bzr", "tag"] + [" ".join(args)])
+if release_name: # TODO: remove test when release detected
+    subprocess.call(["bzr", "tag", release_name])
 
-# now pull and push, but wait for staging to work again.
+# TODO: handle bzr rm
+
+# check if pull branch is set
+bzr_instance = subprocess.Popen(["bzr", "info"], stdout=subprocess.PIPE)
+bzr_info = bzr_instance.stdout.read()
+
+# TODO: see if we want a strategy to set main branch in the project
+
+bzr_staging = ""
+if ("staging" in os.getenv('QUICKLY').lower()):
+    bzr_staging = "//staging/"
+
+branch_location = []
+# if no branch, create it in ~user_name/project_name/quickly_trunk
+# or switch from staging to production
+if not ("parent branch" in bzr_info) or ((".staging." in bzr_info) and not bzr_staging) or (not (".staging." in bzr_info) and bzr_staging):
+
+    branch_location = ['lp:', bzr_staging, '~', launchpad.me.name, '/', project.name, '/quickly_trunk']
+    return_code = subprocess.call(["bzr", "push", "--remember", "".join(branch_location)])
+    if return_code != 0:
+        print _("ERROR: quickly can't release: can't push to launchpad.")
+        sys.exit(1)
+    
+    # make first pull too
+    return_code = subprocess.call(["bzr", "pull", "--remember", "".join(branch_location)])
+    if return_code != 0:
+        print _("ERROR: quickly can't release correctly: can't pull from launchpad.")
+        sys.exit(1)
+    
+else:
+
+    return_code = subprocess.call(["bzr", "pull"])
+    if return_code != 0:
+        print _("ERROR: quickly can't release: can't pull from launchpad.")
+        sys.exit(1)
+        
+    subprocess.call(["bzr", "push"])
+    if return_code != 0:
+        print _("ERROR: quickly can't release: can't push to launchpad.")
+        sys.exit(1)
+
+print _("%s released!") % release_name
+
+# as launchpad-open doesn't support staging server, put an url
+if bzr_staging:
+    webbrowser.open('https://code.staging.launchpad.net/~' + launchpad.me.name + '/' + project.name + '/quickly_trunk')
+else:
+    subprocess.call(["bzr", "launchpad-open"])
+
--- templates/ubuntu-project/save.py
+++ templates/ubuntu-project/save.py
@@ -16,13 +16,14 @@ from gettext import gettext as _
 gettext.textdomain('quickly')
 
 #set either a default message or the specified message
-commit_msg = " ".join(sys.argv[2:])
+commit_msg = " ".join(sys.argv[1:])
 if commit_msg == "":
- commit_msg = _('quickly saved')
+   commit_msg = _('quickly saved')
 
 print commit_msg
 
 #save away
 subprocess.call(["bzr", "add"])
-subprocess.call(["bzr", "commit", "-m" + commit_msg])
+return_code = subprocess.call(["bzr", "commit", "-m" + commit_msg])
 
+return(return_code)
