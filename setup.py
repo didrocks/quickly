b1bdd31006da995c892d920e4d00df848e202f6d
Didier Roche <didrocks@ubuntu.com>
2009-07-13 19:43:38 +0200

    distutils should now works correctly

--- setup.py
+++ setup.py
@@ -31,8 +31,25 @@ def list_files(path_from_root):
             list_files.append(os.path.join(root, file).replace(root_setup, ''))
     return list_files
 
+def return_path_files(path_from_root):
 
+    list_path_file = []
+    root_setup = os.path.abspath('.') + "/"
+    for root, dirs, files in os.walk(os.path.abspath(path_from_root)):
+        list_files = []
+        for file in files:
+            path_to_file = root.replace(root_setup, '')
+            list_files.append(os.path.join(path_to_file, file))
+        if list_files:
+            list_path_file.append((path_to_file, list_files))
+    return list_path_file
 
+def list_data_files():
+
+    data_files=[('/etc/bash_completion.d', ['completion/bash/quickly']),
+                  ('po', list_files('po'))]
+    data_files.extend(return_path_files('templates'))
+    return data_files
 
 
 setup(name='quickly',
@@ -42,8 +59,7 @@ setup(name='quickly',
       author_email='quickly@lists.launchpad.net',
       url='https://launchpad.net/quickly',
       packages=['quickly'],
-      data_files=[('/etc/bash_completion.d', ['completion/bash/quickly']),
-                   ('quickly', list_files('templates'))],
+      data_files=list_data_files(),
       scripts=['src/quickly'],
       cmdclass = { "build" : build_extra.build_extra,
                    "build_i18n" :  build_i18n.build_i18n }
