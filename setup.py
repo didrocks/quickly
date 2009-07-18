807cad568121e494f36110f78c3fea89931518b8
Didier Roche <didrocks@ubuntu.com>
2009-07-18 14:13:38 +0200

    * Make the project comply with distutilsextra.auto and repackaged with   python-mkdebian (will be changed in next commit to separate the   ubuntu-project template)

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-#
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # quickly: quickly project handler
 #
-# Copyright (C) 2009 Canonical Ltd.
+# Copyright (C) 2009 Canonical Ltd.fds
+# Copyright (C) 2009 Rick Spencer
 # Copyright (C) 2009 Didier Roche
+# Copyright (C) 2009 Ken VanDine
+# Copyright (C) 2009 Loïc Minier
+# Copyright (C) 2009 Martin Albisetti   	 
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License version 3,
@@ -16,52 +21,26 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    import sys
+    print >> sys.stderr, 'To build Apport you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
 
-from distutils.core import setup
-from DistUtilsExtra.command import *
-import os
-
-
-def list_files(path_from_root):
-
-    list_files = []
-    root_setup = os.path.abspath('.') + "/"
-    for root, dirs, files in os.walk(os.path.abspath(path_from_root)):
-        for file in files:
-            list_files.append(os.path.join(root, file).replace(root_setup, ''))
-    return list_files
-
-def return_path_files(path_from_root):
-
-    list_path_file = []
-    root_setup = os.path.abspath('.') + "/"
-    for root, dirs, files in os.walk(os.path.abspath(path_from_root)):
-        list_files = []
-        for file in files:
-            path_to_file = root.replace(root_setup, '')
-            list_files.append(os.path.join(path_to_file, file))
-        if list_files:
-            list_path_file.append((path_to_file, list_files))
-    return list_path_file
-
-def list_data_files():
-
-    data_files=[('/etc/bash_completion.d', ['completion/bash/quickly']),
-                  ('po', list_files('po'))]
-    data_files.extend(return_path_files('templates'))
-    return data_files
+assert DistUtilsExtra.auto.__version__ >= '2.6', 'needs DistUtilsExtra.auto >= 2.6'
 
 
-setup(name='quickly',
+DistUtilsExtra.auto.setup(name='quickly',
       version='0.1',
-      description='quickly project handler',
-      author='quickly dev team',
-      author_email='quickly@lists.launchpad.net',
+      description='build new Ubuntu apps quickly',
+      long_description='Quickly enables for prospective programmer a way to build easily new ' \
+                  'apps for Ubuntu based on templates and other systems for helping them ' \
+                  'write their code in a guided manner. This also includes packaging and ' \
+                  'deploying code.',
       url='https://launchpad.net/quickly',
-      packages=['quickly'],
-      data_files=list_data_files(),
-      scripts=['src/quickly'],
-      cmdclass = { "build" : build_extra.build_extra,
-                   "build_i18n" :  build_i18n.build_i18n }
+      license="GPL v3",
+      author='Quickly Developer Team',
+      author_email='quickly@lists.launchpad.net',
       )
 
