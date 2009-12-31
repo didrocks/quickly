29e150671c22c665c4f2ffaf26951257bd6bf84c
Jonathan Lange <jml@canonical.com>
2009-12-31 12:55:27 +1100

    More spelling fixes and drop the double under.

--- data/templates/ubuntu-application/python/Preferencescamel_case_nameDialog.py
+++ data/templates/ubuntu-application/python/Preferencescamel_case_nameDialog.py
@@ -12,7 +12,7 @@ from python_name.helpers import make_window
 
 class Preferencescamel_case_nameDialog(gtk.Dialog):
     __gtype_name__ = "Preferencescamel_case_nameDialog"
-    prefernces = {}
+    preferences = {}
 
     def __init__(self):
         """Construct a Preferencescamel_case_nameDialog.
@@ -41,16 +41,16 @@ class Preferencescamel_case_nameDialog(gtk.Dialog):
         self.builder.connect_signals(self)
 
         # Set up couchdb and the preference info.
-        self.__db_name = "project_name"
-        self.__database = CouchDatabase(self.__db_name, create=True)
-        self.__preferences = None
-        self.__key = None
+        self._db_name = "project_name"
+        self._database = CouchDatabase(self._db_name, create=True)
+        self._preferences = None
+        self._key = None
 
         # Set the record type and then initalize the preferences.
-        self.__record_type = (
+        self._record_type = (
             "http://wiki.ubuntu.com/Quickly/RecordTypes/camel_case_name/"
             "Preferences")
-        self.__preferences = self.get_preferences()
+        self._preferences = self.get_preferences()
         # TODO: code for other initialization actions should be added here
 
     def get_preferences(self):
@@ -58,31 +58,31 @@ class Preferencescamel_case_nameDialog(gtk.Dialog):
 
         Creates a couchdb record if necessary.
         """
-        if self.__preferences == None:
+        if self._preferences == None:
             # The dialog is initializing.
-            self.__load_preferences()
+            self._load_preferences()
 
         # If there were no saved preference, this.
-        return self.__preferences
+        return self._preferences
 
-    def __load_preferences(self):
-        # TODO: add prefernces to the self.__preferences dict default
+    def _load_preferences(self):
+        # TODO: add preferences to the self._preferences dict default
         # preferences that will be overwritten if some are saved
-        self.__preferences = {"record_type": self.__record_type}
+        self._preferences = {"record_type": self._record_type}
 
-        results = self.__database.get_records(
-            record_type=self.__record_type, create_view=True)
+        results = self._database.get_records(
+            record_type=self._record_type, create_view=True)
 
         if len(results.rows) == 0:
             # No preferences have ever been saved, save them before returning.
-            self.__key = self.__database.put_record(Record(self.__preferences))
+            self._key = self._database.put_record(Record(self._preferences))
         else:
-            self.__preferences = results.rows[0].value
-            del self.__preferences['_rev']
-            self.__key = results.rows[0].value["_id"]
+            self._preferences = results.rows[0].value
+            del self._preferences['_rev']
+            self._key = results.rows[0].value["_id"]
 
-    def __save_preferences(self):
-        self.__database.update_fields(self.__key, self.__preferences)
+    def _save_preferences(self):
+        self._database.update_fields(self._key, self._preferences)
 
     def ok(self, widget, data=None):
         """The user has elected to save the changes.
@@ -90,16 +90,16 @@ class Preferencescamel_case_nameDialog(gtk.Dialog):
         Called before the dialog returns gtk.RESONSE_OK from run().
         """
 
-        # Make any updates to self.__preferences here. e.g.
-        #self.__preferences["preference1"] = "value2"
-        self.__save_preferences()
+        # Make any updates to self._preferences here. e.g.
+        #self._preferences["preference1"] = "value2"
+        self._save_preferences()
 
     def cancel(self, widget, data=None):
         """The user has elected cancel changes.
 
         Called before the dialog returns gtk.RESPONSE_CANCEL for run()
         """
-        # Restore any changes to self.__preferences here.
+        # Restore any changes to self._preferences here.
         pass
 
 
