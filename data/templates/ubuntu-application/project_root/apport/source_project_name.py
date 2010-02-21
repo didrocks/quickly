28570f414313224eab9f47c02cd42e0395aafda0
Philip Peitsch <philip.peitsch@gmail.com>
2010-02-21 21:39:48 +1100

    Fixing up file licenses

--- data/templates/ubuntu-application/project_root/apport/source_project_name.py
+++ data/templates/ubuntu-application/project_root/apport/source_project_name.py
@@ -1,16 +1,9 @@
 # Apport integration for project_name
 #
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 3, as published
-# by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranties of
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
-# PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
 import apport
 
 def add_info(report):
