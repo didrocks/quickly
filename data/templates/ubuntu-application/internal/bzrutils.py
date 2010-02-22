07dba5611e55834013a370c3f32412efc31926b5
Philip Peitsch <philip.peitsch@gmail.com>
2010-02-22 21:41:37 +1100

    Adding extra checks to properly handle unchanged files

--- data/templates/ubuntu-application/internal/bzrutils.py
+++ data/templates/ubuntu-application/internal/bzrutils.py
@@ -2,5 +2,7 @@
 from subprocess import Popen, PIPE
 
 def is_file_versioned(v_file):
-    output = Popen(["bzr","status","--short",v_file], stdout=PIPE, stderr=PIPE).communicate()[0]
-    return len(output) > 0 and not output[0] == '?'
+    process = Popen(["bzr","status","--short",v_file], stdout=PIPE, stderr=PIPE).communicate()
+    output = process[0]
+    err = process[1]
+    return len(err) == 0 and (len(output) == 0 or not output[0] == '?')
