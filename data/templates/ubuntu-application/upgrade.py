b1b6278e3ac94eab9ac7928f075d6e02f08baa46
Philip Peitsch <philip.peitsch@gmail.com>
2010-02-27 02:06:14 +1100

    Attempting to fix unneccesary upgrade changes

--- data/templates/ubuntu-application/upgrade.py
+++ data/templates/ubuntu-application/upgrade.py
@@ -4,16 +4,16 @@
 #
 # This file is part of Quickly ubuntu-application template
 #
-#This program is free software: you can redistribute it and/or modify it
-#under the terms of the GNU General Public License version 3, as published
+#This program is free software: you can redistribute it and/or modify it 
+#under the terms of the GNU General Public License version 3, as published 
 #by the Free Software Foundation.
 
-#This program is distributed in the hope that it will be useful, but
-#WITHOUT ANY WARRANTY; without even the implied warranties of
-#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#This program is distributed in the hope that it will be useful, but 
+#WITHOUT ANY WARRANTY; without even the implied warranties of 
+#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
 #PURPOSE.  See the GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License along
+#You should have received a copy of the GNU General Public License along 
 #with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
@@ -106,5 +106,5 @@ if project_version < '0.4':
     lp_project_name = configurationhandler.project_config.get('lp_id', None)
     if lp_project_name is not None:
         internal.apportutils.update_apport(project_name, lp_project_name, lp_project_name)
-
+        
 sys.exit(0)
