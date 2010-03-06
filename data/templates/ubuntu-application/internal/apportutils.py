3ab5afa8db06848691ffb3fd4749d69280124526
Philip Peitsch <philip.peitsch@gmail.com>
2010-03-06 15:40:51 +1100

    Replacing all path references and manual joins with calls to os.path

--- data/templates/ubuntu-application/internal/apportutils.py
+++ data/templates/ubuntu-application/internal/apportutils.py
@@ -17,44 +17,50 @@ def update_apport(project_name, old_lp_project, new_lp_project):
     hook_file = "source_%s.py"%project_name
 
     pathname = templatetools.get_template_path_from_project()
-    template_pr_path = os.path.abspath(pathname) + "/project_root/"
-    relative_etc_dir = "etc/apport/crashdb.conf.d"
+    template_pr_path = os.path.join(os.path.abspath(pathname),"project_root")
+    relative_crashdb_dir = os.path.join("etc", "apport", "crashdb.conf.d")
     relative_apport_dir = "apport"
+    
+    existing_crashdb = os.path.join(relative_crashdb_dir, crashdb_file)
+    existing_hook = os.path.join(relative_apport_dir, hook_file)
+    
+    template_crashdb_dir = os.path.join(template_pr_path, relative_crashdb_dir)
+    template_hook_dir = os.path.join(template_pr_path, relative_apport_dir)
 
     # if the project name has changed, or any of the files are missing, then
     # attempt to set up the apport configuration and hooks
     if  not old_lp_project == new_lp_project \
-        or not os.path.isfile(relative_etc_dir + '/' + crashdb_file) \
-        or not os.path.isfile(relative_apport_dir + '/' + hook_file):
+        or not os.path.isfile(existing_crashdb) \
+        or not os.path.isfile(existing_hook):
 
         subst_existing = ((old_lp_project, new_lp_project),)
         subst_new = (   ("safe_project_name", safe_project_name),
                         ("project_name", project_name),
                         ("lp_project", new_lp_project))
 
-        if os.path.isfile(relative_etc_dir + '/' + crashdb_file):
+        if os.path.isfile(existing_crashdb):
             print _("Updating project name references in existing apport crashdb configuration")
-            quicklyutils.file_from_template(relative_etc_dir + '/', crashdb_file, relative_etc_dir, subst_existing)
-        elif os.path.isdir(template_pr_path + relative_etc_dir):
+            quicklyutils.file_from_template(existing_crashdb, relative_crashdb_dir, subst_existing)
+        elif os.path.isdir(template_crashdb_dir):
             print _("Creating new apport crashdb configuration")
-            if not os.path.isdir(relative_etc_dir):
-                os.makedirs(relative_etc_dir)
-            quicklyutils.file_from_template(template_pr_path + relative_etc_dir + '/', "project_name-crashdb.conf", relative_etc_dir, subst_new)
+            if not os.path.isdir(relative_crashdb_dir):
+                os.makedirs(relative_crashdb_dir)
+            quicklyutils.file_from_template(template_crashdb_dir, "project_name-crashdb.conf", relative_crashdb_dir, subst_new)
 
-        if not os.path.isfile(relative_apport_dir + '/' + hook_file) and os.path.isdir(template_pr_path + relative_apport_dir):
+        if not os.path.isfile(existing_hook) and os.path.isdir(template_hook_dir):
             print _("Creating new apport hooks")
             if not os.path.isdir(relative_apport_dir):
                 os.makedirs(relative_apport_dir)
-            quicklyutils.file_from_template(template_pr_path + relative_apport_dir+ '/', "source_project_name.py", relative_apport_dir, subst_new)
+            quicklyutils.file_from_template(template_hook_dir, "source_project_name.py", relative_apport_dir, subst_new)
 
         print _("Updating launchpad integration references in existing application")
         old_lpi_line = LPI_LINE%old_lp_project
         old_lpi_pn_line = LPI_LINE%project_name
         new_lpi_line = LPI_LINE%new_lp_project
-        for root, dirs, files in os.walk('./'):
+        for root, dirs, files in os.walk('.'):
             for name in files:
                 line_replaced = False
-                if name.endswith('.py') or root == "./bin":
+                if name.endswith('.py') or root == os.path.join(".","bin"):
                     target_file_name = os.path.join(root, name)
                     ftarget_file_name = file(target_file_name, 'r')
                     ftarget_file_name_out = file(ftarget_file_name.name + '.new', 'w')
