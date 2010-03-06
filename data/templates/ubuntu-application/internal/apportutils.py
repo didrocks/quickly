0debcc9475b7fe9aa9b5d9518df4725f789d989e
Philip Peitsch <philip.peitsch@gmail.com>
2010-03-06 16:46:50 +1100

    lpi integration name is package name not launchpad name

--- data/templates/ubuntu-application/internal/apportutils.py
+++ data/templates/ubuntu-application/internal/apportutils.py
@@ -6,8 +6,6 @@ from gettext import gettext as _
 from quickly import templatetools, configurationhandler
 import quicklyutils
 
-LPI_LINE = "LaunchpadIntegration.set_sourcepackagename('%s')"
-
 def update_apport(project_name, old_lp_project, new_lp_project):
     if not new_lp_project:
         return
@@ -52,33 +50,3 @@ def update_apport(project_name, old_lp_project, new_lp_project):
             if not os.path.isdir(relative_apport_dir):
                 os.makedirs(relative_apport_dir)
             quicklyutils.file_from_template(template_hook_dir, "source_project_name.py", relative_apport_dir, subst_new)
-
-        print _("Updating launchpad integration references in existing application")
-        old_lpi_line = LPI_LINE%old_lp_project
-        old_lpi_pn_line = LPI_LINE%project_name
-        new_lpi_line = LPI_LINE%new_lp_project
-        for root, dirs, files in os.walk('.'):
-            for name in files:
-                line_replaced = False
-                if name.endswith('.py') or root == os.path.join(".","bin"):
-                    target_file_name = os.path.join(root, name)
-                    ftarget_file_name = file(target_file_name, 'r')
-                    ftarget_file_name_out = file(ftarget_file_name.name + '.new', 'w')
-                    for line in ftarget_file_name:
-                        # seek the old launchpad integration line and replace it with the new
-                        if old_lpi_line in line:
-                            line_replaced = True
-                            ftarget_file_name_out.write(line.replace(old_lpi_line, new_lpi_line)) # write this line, otherwise will be skipped
-                        elif old_lpi_pn_line in line:
-                            line_replaced = True
-                            ftarget_file_name_out.write(line.replace(old_lpi_pn_line, new_lpi_line)) # write this line, otherwise will be skipped
-                        else:
-                            ftarget_file_name_out.write(line) # write this line, otherwise will be skipped
-
-                    ftarget_file_name.close()
-                    ftarget_file_name_out.close()
-                    if not line_replaced: # that means we didn't find the LPI_LINE, don't copy the file
-                        os.remove(ftarget_file_name_out.name)
-                    else:
-                        templatetools.apply_file_rights(ftarget_file_name.name, ftarget_file_name_out.name)
-                        os.rename(ftarget_file_name_out.name, ftarget_file_name.name)
