5cd09412e06dc90595e3d9ca01f44f016f50f5eb
Rick Spencer <rick.spencer@canonical.com>
2010-03-13 11:09:45 -0800

    copied in the setup.py from the ubuntu-application template

--- data/templates/ubuntu-pygame/project_root/setup.py
+++ data/templates/ubuntu-pygame/project_root/setup.py
@@ -1,42 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ### BEGIN LICENSE
-# Copyright (C) 2010 Rick Spencer <rick.spencer@canonical.com>
-#This program is free software: you can redistribute it and/or modify it 
-#under the terms of the GNU General Public License version 3, as published 
-#by the Free Software Foundation.
-#
-#This program is distributed in the hope that it will be useful, but 
-#WITHOUT ANY WARRANTY; without even the implied warranties of 
-#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
-#PURPOSE.  See the GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License along 
-#with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This file is in the public domain
 ### END LICENSE
 
 ###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
 
+import os
+import sys
+
 try:
     import DistUtilsExtra.auto
 except ImportError:
-    import sys
     print >> sys.stderr, 'To build project_name you need https://launchpad.net/python-distutils-extra'
     sys.exit(1)
-
 assert DistUtilsExtra.auto.__version__ >= '2.10', 'needs DistUtilsExtra.auto >= 2.10'
-import os
-
 
 def update_data_path(prefix, oldvalue=None):
 
     try:
-        fin = file('project_name/project_name_config.py', 'r')
+        fin = file('python_name/python_nameconfig.py', 'r')
         fout = file(fin.name + '.new', 'w')
 
         for line in fin:            
             fields = line.split(' = ') # Separate variable from value
-            if fields[0] == '__project_name_data_directory__':
+            if fields[0] == '__python_name_data_directory__':
                 # update to prefix, store oldvalue
                 if not oldvalue:
                     oldvalue = fields[1]
@@ -50,7 +38,7 @@ def update_data_path(prefix, oldvalue=None):
         fin.close()
         os.rename(fout.name, fin.name)
     except (OSError, IOError), e:
-        print ("ERROR: Can't find project_name/project_name_config.py")
+        print ("ERROR: Can't find python_name/python_nameconfig.py")
         sys.exit(1)
     return oldvalue
 
@@ -76,10 +64,6 @@ def update_desktop_file(datadir):
 
 class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
     def run(self):
-        if self.root or self.home:
-            print "WARNING: You don't use a standard --prefix installation, take care that you eventually " \
-            "need to update quickly/quicklyconfig.py file to adjust __quickly_data_directory__. You can " \
-            "ignore this warning if you are packaging and uses --prefix."
         previous_value = update_data_path(self.prefix + '/share/project_name/')
         update_desktop_file(self.prefix + '/share/project_name/')
         DistUtilsExtra.auto.install_auto.run(self)
@@ -93,10 +77,10 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='project_name',
-    version='0.2~public7',
-    license='GPL-3',
-    author='Rick Spencer',
-    author_email='rick.spencer@canonical.com',
+    version='0.1',
+    #license='GPL-3',
+    #author='Your Name',
+    #author_email='email@ubuntu.com',
     #description='UI for managing …',
     #long_description='Here a longer description',
     #url='https://launchpad.net/project_name',
