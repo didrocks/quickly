96f6c04ae0f37e8ffde8aa863e4752c5a26cd8b2
Rick Spencer <rick.spencer@canonical.com>
2010-03-14 10:11:56 -0700

    fix bug #538623 by adding explosion to the enemy.py

--- data/templates/ubuntu-pygame/project_root/python/enemy.py
+++ data/templates/ubuntu-pygame/project_root/python/enemy.py
@@ -27,6 +27,43 @@ class Enemy(BaseSprite):
         self.rotatingRight = False
         self.rotatingLeft = False
         self.rotationRate = 0
+        self.explosion_sound = pygame.mixer.Sound(project_name_config.enemy_explode_sound)
+        self.explode_stage = 0
+        self.exploding = False
+        self.init_position()
         self.velocity_x = 0
         self.velocity_y = 0
-        self.init_position()
+
+    def update(self):
+        BaseSprite.update(self)
+        if self.exploding: 
+            #do an explosion image for each tick
+            self.explode_stage += 1
+            e = self.explode_stage
+            if e < 8:
+                e = str(e)
+                img_name = project_name_config.enemy_explode_stage + e  + ".png"
+                self.master_image = pygame.image.load(img_name)
+                self._update_image()
+                return
+
+            else:#explosion is done
+                self.visible = False
+                self.exploding = False
+                self.kill()
+                return
+
+
+    def explode(self):
+        """explode - called when enemy is destroyed in
+        game play. Sets the alive flag to False, and sets a flag to
+        start animating an explosion in the update function. Also
+        plays the explosion sound.
+
+        """
+
+        if self.alive:
+            self.explosion_sound.play()
+            self.alive = False
+            self.exploding = True
+
