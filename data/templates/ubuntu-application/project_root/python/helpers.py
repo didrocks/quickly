4a87076c80dd454187e646bc271e8a2da38649dc
David D Lowe <daviddlowe.flimm@gmail.com>
2010-03-15 16:43:17 +0000

    Delete NewClass methods by using special method __new__ in ubuntu-application template.

--- data/templates/ubuntu-application/project_root/python/helpers.py
+++ data/templates/ubuntu-application/project_root/python/helpers.py
@@ -18,12 +18,12 @@ import gettext
 from gettext import gettext as _
 gettext.textdomain('project_name')
 
-def make_window(builder_file_name, window_name):
-    """Return a fully-instantiated window or dialog.
-
+def get_builder(builder_file_name):
+    """Return a fully-instantiated gtk.Builder instance from specified ui 
+    file
+    
     :param builder_file_name: The name of the builder file, without extension.
         Assumed to be in the 'ui' directory under the data path.
-    :param window_name: The name of the window or dialog in the builder file.
     """
     # Look for the ui file that describes the user interface.
     ui_filename = get_data_file('ui', '%s.ui' % (builder_file_name,))
@@ -33,6 +33,4 @@ def make_window(builder_file_name, window_name):
     builder = gtk.Builder()
     builder.set_translation_domain('project_name')
     builder.add_from_file(ui_filename)
-    dialog = builder.get_object(window_name)
-    dialog.finish_initializing(builder)
-    return dialog
+    return builder
