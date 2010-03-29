fb020a037e49e9840498d111c44b8fde92971251
Didier Roche <didrocks@ubuntu.com>
2010-03-29 10:24:58 +0200

    separate stdout domain and stderr domain

--- data/templates/ubuntu-application/internal/packaging.py
+++ data/templates/ubuntu-application/internal/packaging.py
@@ -108,7 +108,7 @@ def _filter_out(line, output_domain, err_output, warn_output):
     if output_domain == DomainLevel.ERROR:
         # only add once an error
         if not line in err_output:
-            err_output.append(line)
+                err_output.append(line)
     elif output_domain == DomainLevel.WARNING:
         # only add once a warning
         if not line in warn_output:
@@ -130,7 +130,8 @@ def _exec_and_log_errors(command, ask_on_warn_or_error=False):
     else:
         proc = subprocess.Popen(command, stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE)
-        output_domain = DomainLevel.NONE
+        stdout_domain = DomainLevel.NONE
+        stderr_domain = DomainLevel.NONE
         err_output = []
         warn_output = []
         while True:
@@ -138,7 +139,7 @@ def _exec_and_log_errors(command, ask_on_warn_or_error=False):
             line_stderr = proc.stderr.readline().rstrip()
             # filter stderr
             if line_stderr:
-                (output_domain, err_output, warn_output) = _filter_out(line_stderr, output_domain, err_output, warn_output)
+                (stderr_domain, err_output, warn_output) = _filter_out(line_stderr, stderr_domain, err_output, warn_output)
 
             if not line_stdout:
                 # don't replace by if proc.poll() as the output can be empty
@@ -146,7 +147,7 @@ def _exec_and_log_errors(command, ask_on_warn_or_error=False):
                     break
             # filter stdout
             else:
-                (output_domain, err_output, warn_output) = _filter_out(line_stdout, output_domain, err_output, warn_output)
+                (stdout_domain, err_output, warn_output) = _filter_out(line_stdout, stdout_domain, err_output, warn_output)
 
         return(_continue_if_errors(err_output, warn_output, proc.returncode,
                                      ask_on_warn_or_error))
