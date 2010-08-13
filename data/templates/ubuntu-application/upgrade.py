e4a0e7c1c8f86fbd48d2548d38c7044252b99fc0
Michael Terry <michael.terry@canonical.com>
2010-08-13 14:59:56 -0400

    add upgrade support for modelines

--- data/templates/ubuntu-application/upgrade.py
+++ data/templates/ubuntu-application/upgrade.py
@@ -163,4 +163,10 @@ if project_version < '0.4.3':
         configurationhandler.project_config['dependencies'] = ", ".join(dependencies)
         configurationhandler.saveConfig()
 
+if project_version < '0.4.4':
+    # Use full modelines for all python files
+    sedline = "sed -i 's/-\*- coding: utf-8 -\*-/-*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-/'"
+    os.system("find . -name '*.py' -exec %s {} \;" % sedline)
+    os.system("%s bin/%s" % (sedline, project_name))
+
 sys.exit(0)
