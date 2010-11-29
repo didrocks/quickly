9fe02a7ab7f043c278f1d66b34dc7305ea4138ec
Michael Terry <mike@mterry.name>
2010-11-29 09:24:53 -0500

    add patch from Tony to notify user of typos

--- data/templates/ubuntu-application/project_root/python/BuilderGlue.py
+++ data/templates/ubuntu-application/project_root/python/BuilderGlue.py
@@ -8,14 +8,23 @@ import gtk
 import string
 import sys
 import inspect
+import logging
 
 class BuilderGlue:
     def __init__(self, builder, callback_obj = None, autoconnect = True):
         "Takes a gtk.Builder and makes all its objects easily accessible"
 
+        unconnected_funcs = []
+
         # Hook up any signals the user defined in glade
         if callback_obj is not None:
             builder.connect_signals(callback_obj)
+
+            # This function list is used below, but created once here
+            funcs = dict(inspect.getmembers(callback_obj, inspect.ismethod))
+
+            # test for typos in event handlers
+            unconnected_funcs = funcs.keys()
 
         # Support 'for o in self'
         def iterator():
@@ -71,11 +80,11 @@ class BuilderGlue:
                 except:
                     pass
                 sigs = [GObject.signal_name(sid) for sid in sig_ids]
-                funcs = dict(inspect.getmembers(callback_obj, inspect.ismethod))
 
                 def connect_if_present(sig, cb_name):
                     if cb_name in funcs:
                         obj.connect(sig, funcs[cb_name])
+                        unconnected_funcs.remove(cb_name)
 
                 # We avoid clearer on_OBJ_SIG pattern, because that is
                 # suggested by glade, and we don't have a way to detect if
@@ -86,3 +95,6 @@ class BuilderGlue:
                     if obj is callback_obj:
                         connect_if_present(sig, "%s_event" % sig)
 
+        unconnected_event_handlers = [x for x in unconnected_funcs if '_event' in x]
+        for event_handler in unconnected_event_handlers:
+            logging.warn('%s.%s looks like a signal handler, but no signal was found to connect to it!' % (callback_obj, event_handler))
