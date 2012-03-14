7d92c59f84174181d2f1cb4328d0753dd66b381a
Michael Terry <michael.terry@canonical.com>
2012-03-14 19:29:01 -0400

    update tutorial

--- data/templates/ubuntu-application/help/code/JottyWindow.py
+++ data/templates/ubuntu-application/help/code/JottyWindow.py
@@ -7,6 +7,8 @@ import gettext
 from gettext import gettext as _
 gettext.textdomain('jotty')
 
+import os
+from gi.repository import GLib # pylint: disable=E0611
 from gi.repository import Gtk # pylint: disable=E0611
 import logging
 logger = logging.getLogger('jotty')
@@ -15,16 +17,11 @@ from jotty_lib import Window
 from jotty.AboutJottyDialog import AboutJottyDialog
 from jotty.PreferencesJottyDialog import PreferencesJottyDialog
 
-from desktopcouch.records.server import CouchDatabase
-from desktopcouch.records.record import Record
-
-# pylint: disable=E1002,E1101
-
 # See jotty_lib.Window.py for more details about how this class works
 class JottyWindow(Window):
     __gtype_name__ = "JottyWindow"
     
-    def finish_initializing(self, builder):
+    def finish_initializing(self, builder): # pylint: disable=E1002
         """Set up the main window"""
         super(JottyWindow, self).finish_initializing(builder)
 
@@ -32,7 +29,6 @@ class JottyWindow(Window):
         self.PreferencesDialog = PreferencesJottyDialog
 
         # Code for other initialization actions should be added here.
-        self.database = CouchDatabase("jotty", create=True)
 
     def on_mnu_save_activate(self, widget, data=None):
         #get the title for the note
@@ -42,38 +38,32 @@ class JottyWindow(Window):
         buff = self.ui.textview1.get_buffer()
         start_iter = buff.get_start_iter()
         end_iter = buff.get_end_iter()
-        text = buff.get_text(start_iter,end_iter)
+        text = buff.get_text(start_iter, end_iter, True)
 
-        #get all the records
-        record_type = "http://wiki.ubuntu.com/Quickly/JottyDoc"
-        results = self.database.get_records(record_type = record_type, create_view = True)
+        #create the filename
+        data_dir = GLib.get_user_data_dir()
+        jotty_dir = os.path.join(data_dir, "jotty")
+        filename = os.path.join(jotty_dir, title)
 
-        #update a record that has the same title
-        for result in results:
-            document = result.value
-            if document["title"] == title:
-                key = document["_id"]
-                self.database.update_fields(key, {"text":text})
-                return
-
-        #if no records had the title, create it 
-        new_rec = Record({"record_type":record_type, "title":title, "text":text})
-        self.database.put_record(new_rec)
+        #write the data
+        GLib.mkdir_with_parents(jotty_dir, 0o700)
+        GLib.file_set_contents(filename, text)
 
     def on_mnu_open_activate(self, widget, data=None):
         #get the name of the document to open
         title = self.ui.entry1.get_text()
         text = ""
- 
-        #get all the records
-        record_type = "http://wiki.ubuntu.com/Quickly/JottyDoc"
-        results = self.database.get_records(record_type = record_type,create_view = True)
- 
-        #get the text if there is a matching title
-        for result in results:
-            document = result.value
-            if document["title"] == title:
-                text = document["text"]
+
+        #create the filename
+        data_dir = GLib.get_user_data_dir()
+        jotty_dir = os.path.join(data_dir, "jotty")
+        filename = os.path.join(jotty_dir, title)
+
+        #try to get the data from the file if it exists
+        try:
+            success, text = GLib.file_get_contents(filename)
+        except Exception:
+            text = ""
 
         #set the UI to display the string
         buff = self.ui.textview1.get_buffer()
